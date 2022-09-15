229ab4407203ba22b13a9970abc86e1b1e992ef7
jm-moore <jmmoore9358@gmail.com>
2022-09-15 11:37:49 -0500

    initial commit

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ console.log(
 );
 
 // Exercise 1
-
+const firstName = "Joshua";
+let lastName = "Moore";
+var age = 24;
 // Exercise 2
-
+fullName = '${firstName} ${lastName}';
+console.log(fullName);
 // Exercise 3
+let city = "Mustang";
+let pastTime = "hiking";
+let myStory = 'Hi! My name is ${fullname}. I live in ${city}. I enjoy ${pastTime} and coding!';
+console.log(myStory);
