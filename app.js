0a543ed8d3d505a80e1ee1e9873f554af8092a81
jm-moore <jmmoore9358@gmail.com>
2022-09-15 11:43:27 -0500

    completed last exercises

--- app.js
+++ app.js
@@ -15,3 +15,21 @@ let city = "Mustang";
 let pastTime = "hiking";
 let myStory = 'Hi! My name is ${fullname}. I live in ${city}. I enjoy ${pastTime} and coding!';
 console.log(myStory);
+
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
